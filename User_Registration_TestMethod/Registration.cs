81a6feb2cff4fd244df7e78b42bd176c30f0fae3
Shridhartadakod <shridhar1491998@gmail.com>
2022-03-06 09:21:58 +0530

    first name is added

---
+++ User_Registration_TestMethod/Registration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Registration_TestMethod
+{
+    public class Registration
+    {
+        public void FirstName()
+        {
+            Console.Write("Enter the name = ");
+            string name = Console.ReadLine();
+            string Pattern = "^[A-Z][a-zA-Z]{2,}$";
+
+            if (Validate(name, Pattern))
+                Console.WriteLine("The entered name is valid!!");
+            else
+                Console.WriteLine("Please enter the valid name..");
+        }
+
+        //Creating method to check validation
+        public bool Validate(string info, string Pattern)
+        {
+            return Regex.IsMatch(info, Pattern);
+        }
+    }
+}
