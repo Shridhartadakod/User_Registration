11f398d12b2199fdc22b3fc42e2e44b7dd261c87
Shridhartadakod <shridhar1491998@gmail.com>
2022-03-06 09:18:56 +0530

    User registration

---
+++ User_Registration_TestMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Registration_TestMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User registration program:");
+        }
+    }
+}
