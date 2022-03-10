1945552be1ee293ce1812a02be603d9979e9544c
Shridhartadakod <shridhar1491998@gmail.com>
2022-03-10 19:13:43 +0530

    reflactoe is added

--- User_Registration_TestMethod/Program.cs
+++ User_Registration_TestMethod/Program.cs
@@ -11,14 +11,18 @@ namespace User_Registration_TestMethod
         static void Main(string[] args)
         {
             Console.WriteLine("User registration program:");
-            Console.WriteLine("==========Welcome To User Registration===========");
-            //Creating object od class
-            Registration registration = new Registration();
-            registration.FirstName();
-            registration.LastName();
-            registration.EmailId();
-            registration.MobileNumber();
-            registration.Password();
+            Console.WriteLine("Using Reflection to enter Registration form for a new user");
+            try
+            {
+                Type type = typeof(Registration);
+                var form = Activator.CreateInstance(type);
+                MethodInfo methodInfo = type.GetMethod("GetInfo");
+                methodInfo.Invoke(form, null);
+            }
+            catch
+            {
+                Console.WriteLine("Reflection failed");
+            }
 
             Console.ReadKey();
         }
